d435e4564f90fbaff49b1578560a99f780666aaf
ls <479825880@qq.com>
2018-11-14 17:29:26 +0800

    scala集合map操作

--- src/main/scala/com/ls/collectiondemo/set/RemoveDemo.scala
+++ src/main/scala/com/ls/collectiondemo/set/RemoveDemo.scala
@@ -14,6 +14,7 @@ object RemoveDemo {
     println(s -- Traversable(2, 3)) //Set(5, 1, 4)
 
     val s2 = mutable.Set(1 to 5: _*)
+    println(s2) //Set(1, 5, 2, 3, 4)
     println(s2.remove(2)) //true
     println(s2) //Set(1, 5, 3, 4)
 
